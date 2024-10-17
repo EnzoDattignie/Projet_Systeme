dac6f0e9e845c8522d4939260e24e9dfaf2d1f82
EnzoD <enzo.dattignie@gmail.com>
2024-10-17 12:43:12 +0200

    Fix : Prise en compte des négatif, Ajout : Fonctions Minimum et Maximum nécessaire pour le programme - Fonction file_here qui vérifie la présence d'un fichier peu importe le   format du path indiqué et donne des messages d'erreur correspondants - Prise en compte du fichier a lire a partir des arguments de Python

--- intensite.py
+++ intensite.py
@@ -1,34 +1,87 @@
+import sys, os
+
 
 #Fonction retournant True si le string donné peut etre converti en float
-def is_number(s):
-    fpoint = False
+def is_number(s) :
+    fpoint = False #Ce flag vérifie que le point est passé ou non, il est set a True si il voit un point et si il voit de nouveau un point le résultat final est false, un float ne possede qu'un seul point
     res = True
-    i = 0
+    i = 1
     if (len(s)==0):
         res = False
-    if (s[0].isdigit() == False) :
-        res = False
-    while ((i < len(s)) and (res == True)) :
-        if (s[i].isdigit() == False):
-            if (s[i] == "." and fpoint == False):
+    else :
+        #un string peut etre converti en float si il commence par un chiffre, un moins, ou un point
+        if s[0].isdigit() or s[0] == "-" or s[0] == "." :
+            if s[0] == ".":
                 fpoint = True
-            else :
-                res = False
-        i = i+1
+            while ((i < len(s)) and (res == True)) :
+                if (s[i].isdigit() == False):
+                    if (s[i] == "." and fpoint == False):
+                        fpoint = True
+                    else :
+                        res = False
+                i = i+1
+        else :
+            res = False
     return res
 
 
 intensite = []
 longueur = []
 
-fd = open("Spectre_photoluminescence.txt","r")
-for ligne in fd :
-    x = ligne.strip().split()
-    #On vérifie bien que deux nombres sont fournis et que les deux sont des nombres
-    if (len(x) == 2 and is_number(x[0]) and is_number(x[1])):
-        longueur.append(float(x[0]))
-        intensite.append(float(x[1]))
-fd.close()
+#Programme retournant l'index de la valeur minimale d'une liste
+def minimum(l) :
+    res = l[0]
+    index = 0
+    for i in range (0,len(l)):
+        if (l[i] < res):
+            res = l[i]
+            index = i
+    return index
 
-print(longueur)
-print(intensite)
+def maximum(l) :
+    res = l[0]
+    index = 0
+    for i in range (0,len(l)):
+        if (l[i] > res):
+            res = l[i]
+            index = i
+    return index
+
+def file_here(path):
+    presence = False
+    if (path[0] == "/" or path[0] == "."):#On est face a un chemin absolu ou relatif
+        path = path.split("/")
+        file = path[len(path)-1]
+        dir = ""
+        for i in range (0, len(path)-1):
+            dir = dir+path[i]+"/"
+        fichiers = os.listdir(dir)
+    else : #On est dans le cas d'un nom de fichier
+        fichiers = os.listdir("./")
+        file = path
+    for f in fichiers : #vérification que le fichier existe bien a l'endroit indiqué
+        if (f == file) :
+            presence = True
+    return presence,file
+
+if (len(sys.argv) > 1) : #On verifie qu'un argument est donné, sinon on ne peut pas ouvrir le fichier
+    presence = file_here(sys.argv[1])
+    if presence[0] == False :
+        print("No file named \"{}\" found".format(presence[1]))
+    else :
+        fd = open(sys.argv[1],"r")
+        for ligne in fd :
+            x = ligne.strip().split()
+            #On vérifie bien que deux nombres sont fournis et que les deux sont des nombres
+            if (len(x) == 2 and is_number(x[0]) and is_number(x[1])):
+                longueur.append(float(x[0]))
+                intensite.append(float(x[1]))
+        fd.close()
+        maxi = maximum(intensite)
+        mini = minimum(intensite)
+        print("intensite max = {} , pour une longueur d'onde de {}".format(intensite[maxi],longueur[maxi]))
+        print("intensite min = {} , pour une longueur d'onde de {}".format(intensite[mini],longueur[mini]))
+else :
+    print("No file indicated, please add a file as an argument")
+
+
