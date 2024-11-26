fc4ca7d436cde415669aee59ae5585ba7cba163c
Enzo <enzo.dattignie@gmail.com>
2024-11-26 11:15:46 +0100

    Unification des messages d'erreurs en francais et correction d'une annotation

--- intensite.py
+++ intensite.py
@@ -13,12 +13,12 @@ if len(sys.argv) > 2: #On verifie qu'une fenetre est bien donnée et que c'est b
 if (len(sys.argv) > 1) : #On verifie qu'un argument est donné, sinon on ne peut pas ouvrir le fichier
     dict = dict_creation(sys.argv[1].strip(),fenetre)
 else :
-    print("No file indicated, please add a file as an argument")
+    print("Pas de fichier indiqué, merci d'indiquer un fichier en argument")
 
 #Retour de toutes les données demandées
 for key in dict :
     dict[key] = sort_list(dict[key])
-    print ("Clef : \"{}\"".format(key))
+    print ("Longueur : \"{}\"".format(key))
     print ("Nb de données : {}".format(len(dict[key])))
     print ("Min : {}".format(dict[key][minimum(dict[key])]))
     print ("Max : {}".format(dict[key][maximum(dict[key])]))
