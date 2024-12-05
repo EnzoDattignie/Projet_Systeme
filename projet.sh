ebaf3199d5ddba945e46761aba01d4371f662664
enzod <enzo.dattignie@gmail.com>
2024-12-05 13:36:14 +0100

    Ajout de la fonction de bouclage

--- projet.sh
+++ projet.sh
@@ -1,15 +1,31 @@
 echo "Arguments :" $@
-echo "Voulez vous graph sur toute l'échantillon ?(y/n)"
-read r1
-echo "Voulez vous sauvegarder les données à la fin de l'exécution ?(y/n)"
-read s1
-if [ $r1 == "y" ]
-then 
-    python3 'intensite.py' $@ | python3 'recherche_plot.py' 'a' 'a' $s1
-else
-    echo "Veuiller indiquer la longueur d'onde minimale de la fenetre"
-    read t1
-    echo "Veuiller indiquer la longueur d'onde maximale de la fenetre"
-    read t2
-    python3 'intensite.py' $@ | python3 'recherche_plot.py' $t1 $t2 $s1
-fi
+flag=True
+boucle=n
+while [ $flag == True ]
+do
+    echo "Voulez vous graph sur toute l'échantillon ?(y/n)"
+    read r1
+    echo "Voulez vous sauvegarder les données à la fin de l'exécution ?(y/n)"
+    read s1
+    t1='null'
+    t2='null'
+    if [ $r1 == "n" ]
+    then
+        echo "Veuiller indiquer la longueur d'onde minimale de la fenetre"
+        read t1
+        echo "Veuiller indiquer la longueur d'onde maximale de la fenetre"
+        read t2
+    fi
+    if [ $boucle == "n" ]
+    then
+        python3 'intensite.py' $@ | python3 'recherche_plot.py' $t1 $t2 $s1 $boucle
+    else
+        python3 'recherche_plot.py' $t1 $t2 $s1 $boucle
+    fi
+    echo "Voulez vous continuer ?(y/n)"
+    read boucle
+    if [ $boucle != "y" ]
+    then
+        flag=False
+    fi
+done
