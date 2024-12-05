57db5d7deda4c9ce108881e04b96d40e7d8316a6
Ahmed <ahmed@macbook-air-de-ahmed.home>
2024-12-05 17:40:46 +0100

    implementationdeis_corrupted

--- intensite.py
+++ intensite.py
@@ -2,25 +2,25 @@ import sys
 from functions import *
 dict = {}
 
+if not(is_corrupted(sys.argv[0],sys.argv[0])):
+    fenetre = 10
+    if len(sys.argv) > 2: #On verifie qu'une fenetre est bien donnée et que c'est bien un nombre, sinon on la laisse a 10
+        if is_number(sys.argv[2].strip()) :
+            fenetre = float(sys.argv[2])
+        else :
+            print("Erreur, fenetre paramétrée a 10nm")
 
-fenetre = 10
-if len(sys.argv) > 2: #On verifie qu'une fenetre est bien donnée et que c'est bien un nombre, sinon on la laisse a 10
-    if is_number(sys.argv[2].strip()) :
-        fenetre = float(sys.argv[2])
+    if (len(sys.argv) > 1) : #On verifie qu'un argument est donné, sinon on ne peut pas ouvrir le fichier
+        dict = dict_creation(sys.argv[1].strip(),fenetre)
     else :
-        print("Erreur, fenetre paramétrée a 10nm")
+        print("Pas de fichier indiqué, merci d'indiquer un fichier en argument")
 
-if (len(sys.argv) > 1) : #On verifie qu'un argument est donné, sinon on ne peut pas ouvrir le fichier
-    dict = dict_creation(sys.argv[1].strip(),fenetre)
-else :
-    print("Pas de fichier indiqué, merci d'indiquer un fichier en argument")
-
-#Retour de toutes les données demandées
-for key in dict :
-    dict[key] = sort_list(dict[key])
-    print ("Longueur : \"{}\"".format(key))
-    print ("Nb de données : {}".format(len(dict[key])))
-    print ("Min : {}".format(dict[key][minimum(dict[key])]))
-    print ("Max : {}".format(dict[key][maximum(dict[key])]))
-    print ("Moyenne : {}".format(average(dict[key])))
-    print ()
+    #Retour de toutes les données demandées
+    for key in dict :
+        dict[key] = sort_list(dict[key])
+        print ("Longueur : \"{}\"".format(key))
+        print ("Nb de données : {}".format(len(dict[key])))
+        print ("Min : {}".format(dict[key][minimum(dict[key])]))
+        print ("Max : {}".format(dict[key][maximum(dict[key])]))
+        print ("Moyenne : {}".format(average(dict[key])))
+        print ()
--- recherche_plot.py
+++ recherche_plot.py
@@ -1,87 +1,89 @@
+from queue import Full
 import sys
 from functions import *
 import matplotlib.pyplot as plt
 import datetime
 
-print(sys.argv)
+stdin_data=list(sys.stdin)    #On créer une liste contenant les valeurs obtenues dans intensite.py
 
-longueur = []
-intensite = []
-
-if sys.argv[4] == "n" :
-    for line in sys.stdin : #Nous lisons le stdin ligne par ligne pour récuperer les données pipe par intensite.py
-        l = line.split()
-        if len(l) >= 3 :
-            print (line.strip()) #On affiche les lignes rendues invisibles ppar le pipe et on garde en longueur d'onde la clef et en intensité la valeur moyenne sur la fenetre
-            if l[0] == "Longueur":
-                longueur.append(float(l[2].strip('"')))
-            if l[0] == 'Moyenne':
-                intensite.append(float(l[2].strip()))
-                print('\n')
-else :
-    with open("temp.txt","r") as temp:
-        for l in temp :
-            line = l.split()
-            print(line)
-            longueur.append(float(line[0]))
-            intensite.append(float(line[1]))
-
-inf = sys.argv[1].strip() 
-sup = sys.argv[2].strip()
-
-if is_number(inf) and is_number(sup): #Si ce sont des nombres on les converti en float sinon on prend le min et le max de longueur d'onde
-    inf_ = float(inf)
-    sup_ = float(sup)
-    if sup_<inf_:          #inversion de sup et inf si sup<inf
-        sup_,inf_=inf_,sup_
+if len(stdin_data)==0:
+    print("Le fichier est corrumpu")
 else:
-    print('Les valeurs rentrées ne sont pas correctes ou inexistantes, plot sur toute la longueur de l\'echantillon')
-    if (len(longueur) > 0) : #Evite juste une erreur si l'échantillon est vide
-        inf_ = longueur[minimum(longueur)]
-        sup_ = longueur[maximum(longueur)]
-    else :
-        inf_ = 0
-        sup_ = 0
-
-#Ces listes seront les listes réelles entre les deux valeurs de longueur d'onde indiquées par l'utilisateur
-longueurf=[] 
-intensitef=[]
-for i in range(len(longueur)):  #Creation d'une liste de longueurs d'onde et d'intensités correspondant à l'intervalle demandé
-    if inf_ <= longueur[i] <=sup_:        
-        longueurf.append(longueur[i])
-        intensitef.append(intensite[i])
-
-#Normalisation de l'intensité
-
-if len(intensite) > 0 :
-    Imax=intensite[maximum(intensite)]
-else :
-    Imax = 0
-intensitef_normalisé=[]
-for i in range(len(intensitef)):
-    intensitef_normalisé.append(intensitef[i]/Imax)
-
-filename = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-plt.plot(longueurf,intensitef_normalisé,label='I(λ)',color='red')
-plt.ylim(-0.10,1.10)
-plt.legend()
-plt.xlabel("Longueur d'onde (λ)")
-plt.ylabel("Intensité (I)")
-plt.title("Graphique représentant l'intensité en fonction de la longueur d'onde")
-
-with open("temp.txt","w") as temp :
-    for i in range(0,len(intensite)) :
-        temp.write("{}\t{}\n".format(longueur[i],intensite[i]))
-
-if len(longueurf)>0: #On verifie que les listes sont remplies avant de plot
-    if sys.argv[3] == 'y':
-        filename = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        with open(filename+"_save.txt",'w') as save :
-            for i in range (0,len(intensitef_normalisé)) :
-                save.write("{}\t{}\n".format(longueurf[i],intensitef_normalisé[i]))
-        plt.savefig(filename)
-    plt.show()
-else:
-    print('Aucune données à afficher sur cet intervalle')
-
+    longueur = []
+    intensite = []
     
+    if sys.argv[4] == "n" :
+        for line in stdin_data : #Nous lisons le stdin ligne par ligne pour récuperer les données pipe par intensite.py
+            l = line.split()
+            if len(l) >= 3 :
+                print (line.strip()) #On affiche les lignes rendues invisibles ppar le pipe et on garde en longueur d'onde la clef et en intensité la valeur moyenne sur la fenetre
+                if l[0] == "Longueur":
+                    longueur.append(float(l[2].strip('"')))
+                if l[0] == 'Moyenne':
+                    intensite.append(float(l[2].strip()))
+                    print('\n')
+    else :
+        with open("temp.txt","r") as temp:
+            for l in temp :
+                line = l.split()
+                print(line)
+                longueur.append(float(line[0]))
+                intensite.append(float(line[1]))
+
+    inf = sys.argv[1].strip() 
+    sup = sys.argv[2].strip()
+
+    if is_number(inf) and is_number(sup): #Si ce sont des nombres on les converti en float sinon on prend le min et le max de longueur d'onde
+        inf_ = float(inf)
+        sup_ = float(sup)
+        if sup_<inf_:          #inversion de sup et inf si sup<inf
+            sup_,inf_=inf_,sup_
+    else:
+        print('Les valeurs rentrées ne sont pas correctes ou inexistantes, plot sur toute la longueur de l\'echantillon')
+        if (len(longueur) > 0) : #Evite juste une erreur si l'échantillon est vide
+            inf_ = longueur[minimum(longueur)]
+            sup_ = longueur[maximum(longueur)]
+        else :
+            inf_ = 0
+            sup_ = 0
+
+    #Ces listes seront les listes réelles entre les deux valeurs de longueur d'onde indiquées par l'utilisateur
+    longueurf=[] 
+    intensitef=[]
+    for i in range(len(longueur)):  #Creation d'une liste de longueurs d'onde et d'intensités correspondant à l'intervalle demandé
+        if inf_ <= longueur[i] <=sup_:        
+            longueurf.append(longueur[i])
+            intensitef.append(intensite[i])
+
+    #Normalisation de l'intensité
+
+    if len(intensite) > 0 :
+        Imax=intensite[maximum(intensite)]
+    else :
+        Imax = 0
+    intensitef_normalisé=[]
+    for i in range(len(intensitef)):
+        intensitef_normalisé.append(intensitef[i]/Imax)
+
+    filename = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    plt.plot(longueurf,intensitef_normalisé,label='I(λ)',color='red')
+    plt.ylim(-0.10,1.10)
+    plt.legend()
+    plt.xlabel("Longueur d'onde (λ)")
+    plt.ylabel("Intensité (I)")
+    plt.title("Graphique représentant l'intensité en fonction de la longueur d'onde")
+
+    with open("temp.txt","w") as temp :
+        for i in range(0,len(intensite)) :
+            temp.write("{}\t{}\n".format(longueur[i],intensite[i]))
+
+    if len(longueurf)>0: #On verifie que les listes sont remplies avant de plot
+        if sys.argv[3] == 'y':
+            filename = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            with open(filename+"_save.txt",'w') as save :
+                for i in range (0,len(intensitef_normalisé)) :
+                    save.write("{}\t{}\n".format(longueurf[i],intensitef_normalisé[i]))
+            plt.savefig(filename)
+        plt.show()
+    else:
+        print('Aucune données à afficher sur cet intervalle')
